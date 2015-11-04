9f62909c9e1289ca7dfade84c536ce8e164d261a
Ankita Dave <adave@lcs-208-124.syr.edu>
2015-11-04 17:14:58 -0500

    Initial Commit

---
+++ FinanceStreet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FinanceStreet
+//
+//  Created by Ankita Dave on 11/4/15.
+//  Copyright © 2015 Ankita Dave. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FinanceStreet/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FinanceStreet
+//
+//  Created by Ankita Dave on 11/4/15.
+//  Copyright © 2015 Ankita Dave. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
